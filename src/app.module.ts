d81516ef75d77bd369e4912fd0f1f0a43b948495
coderoycc <rcchambi4@gmail.com>
2025-04-08 20:54:43 -0400

    init: module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MiModuleModule } from './mi-module/mi-module.module';
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MiModuleModule, CoreModule],
+  imports: [MiModuleModule, CoreModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
