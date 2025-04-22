65ff2575f0d1596f17cddd20a7a26a5cf5117408
coderoycc <rcchambi4@gmail.com>
2025-04-21 22:33:30 -0400

    init: notification module

--- src/notification/domain/entities/notification.ts
+++ src/notification/domain/entities/notification.ts
@@ -1,3 +1,13 @@
+import { Target } from "@target/domain/entities/target";
+import { Template } from "@template/domain/entities/template";
+
 export class Notification {
+  id: string;
+  target: Target;
+  message: string;
+  dateToSend: Date;
+  programable: boolean;
+  status: string;
+  template: Template;
   constructor() { }
 }
