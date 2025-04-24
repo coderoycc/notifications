76ac267c23dd81d56572353fdb3708c1ec3e4935
coderoycc <rcchambi4@gmail.com>
2025-04-23 22:47:04 -0400

    init: rechange

--- src/notification/domain/entities/notification.ts
+++ src/notification/domain/entities/notification.ts
@@ -2,8 +2,8 @@ import { Target } from "@target/domain/entities/target";
 import { Template } from "@template/domain/entities/template";
 import { NotificationStatus } from "./status.enum";
 
-export class Notification {
-  id: string;
+export abstract class Notification {
+  id?: string;
   target: Target;
   title: string;
   message: string;
@@ -13,21 +13,10 @@ export class Notification {
   status: NotificationStatus;
   createdAt: Date;
   template: Template | null;
+}
 
-  constructor() { }
-  validate(): boolean {
-    if (!this.target) {
-      throw new Error("Target is required");
-    }
-    if (!this.title) {
-      throw new Error("Title is required");
-    }
-    if (!this.message) {
-      throw new Error("Message is required");
-    }
-    if (this.scheduled && !this.dateToSend) {
-      throw new Error("Date to send is required when scheduled");
-    }
-    return true;
-  }
+export interface NotificationSender {
+  beforeSend(notification: Notification): Promise<void>;
+  send(notification: Notification): Promise<NotificationStatus>;
+  afterSend(notification: Notification): Promise<void>;
 }
