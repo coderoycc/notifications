49ca6f4e79fd3d6670ee59571857f3e49d6d24a5
rchambi <>
2025-05-08 23:07:08 -0400

    Add notification module and repository: implement NotificationModule with CreateNotificationUseCase and NotificationTypeOrmRepository; introduce NotificationNotPermissionsException for permission handling.

--- src/notification/domain/exceptions/notification.exceptions.ts
+++ src/notification/domain/exceptions/notification.exceptions.ts
@@ -1,3 +1,5 @@
+import { HttpException } from "@nestjs/common";
+
  export class NotificationNotFoundException extends Error {
   constructor(notificationId: string) {
     super(`Notification with ID ${notificationId} not found`);
@@ -9,5 +11,12 @@ export class NotificationTargetNotValidException extends Error {
   constructor(target: string) {
     super(`Notification target ${target} is not valid`);
     this.name = 'NotificationTargetNotValidException';
+  }
+}
+
+export class NotificationNotPermissionsException extends HttpException {
+  constructor() {
+    super('You do not have permission to perform this action', 403);
+    this.name = 'NotificationNotPermissionsException';
   }
 }
