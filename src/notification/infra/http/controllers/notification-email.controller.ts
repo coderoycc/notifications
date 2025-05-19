42145fb785107e7f7847f0e90bc953b274deef7b
rchambi <>
2025-05-18 21:39:48 -0400

    fix: validate scheduled date in scheduleNotification method

--- src/notification/infra/http/controllers/notification-email.controller.ts
+++ src/notification/infra/http/controllers/notification-email.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, HttpException, HttpStatus, Get, Query } from '@
 import { CreateNotificationDto } from '../dtos/create-notification.dto';
 import { NotificationEmailMapper } from '../mappers/notification-email.mapper';
 import { CreateNotificationUseCaseImpl } from '@noti-app/use-cases/create-notification.use-case';
+import moment from 'moment';
 
 @Controller('notification')
 export class NotificationEmailCreateController {
@@ -30,12 +31,19 @@ export class NotificationEmailCreateController {
   @Post('schedule-email')
   async scheduleNotification(@Body() createNotificationDto: Required<CreateNotificationDto>) {
     try {
+      if (
+        !moment(createNotificationDto.scheduledAt, 'YYYY-MM-DD HH:mm:ss', true).isValid()
+        || moment(createNotificationDto.scheduledAt).isBefore(moment()
+      )
+      ) {
+        throw new Error('Scheduled date is not valid');
+      }
       const dataToCreate = NotificationEmailMapper.toCreateRequestWithSchedule(createNotificationDto);
       const notification = await this.notificationCreateService.execute(dataToCreate);
       return { success: true, data: notification };
     } catch (error) {
       throw new HttpException(
-        { success: false, message: error.message },
+        { success: false, message: error.message || 'Unknown request error' },
         HttpStatus.BAD_REQUEST,
       )
     } 
