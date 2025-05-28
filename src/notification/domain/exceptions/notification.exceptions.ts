9481c5b7f75b23a66a7032106d5958369344d9a0
rchambi <>
2025-05-27 22:46:18 -0400

    feat: introduce ExceptionCodes enum and refactor notification exceptions for improved error handling

--- src/notification/domain/exceptions/notification.exceptions.ts
+++ src/notification/domain/exceptions/notification.exceptions.ts
@@ -1,29 +1,43 @@
-import { BadRequestException, HttpException } from "@nestjs/common";
+import { ExceptionCodes } from "./exception-codes";
 
- export class NotificationNotFoundException extends Error {
+export class NotificationError extends Error {
+  constructor(message: string, public readonly code: string) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export class NotificationNotFoundException extends NotificationError {
   constructor(notificationId: string) {
-    super(`Notification with ID ${notificationId} not found`);
+    super(`Notification with ID ${notificationId} not found`, ExceptionCodes.NOT_FOUND);
     this.name = 'NotificationNotFoundException';
   }
 }
 
-export class NotificationTargetNotValidException extends Error {
+export class NotificationTargetNotValidException extends NotificationError {
   constructor(target: string) {
-    super(`Notification target ${target} is not valid`);
+    super(`Notification target ${target} is not valid`, ExceptionCodes.VALIDATION_ERROR);
     this.name = 'NotificationTargetNotValidException';
   }
 }
 
-export class NotificationNotPermissionsException extends HttpException {
+export class NotificationNotPermissionsException extends NotificationError {
   constructor() {
-    super('You do not have permission to perform this action', 403);
+    super('You do not have permission to perform this action', ExceptionCodes.PERMISSION_DENIED);
     this.name = 'NotificationNotPermissionsException';
   }
 }
 
-export class SenderQuotaExceededException extends BadRequestException {
+export class SenderQuotaExceededException extends NotificationError {
   constructor(sender: string, quota: number) {
-    super(`Sender ${sender} has exceeded the quota of ${quota} notifications`);
+    super(`Sender ${sender} has exceeded the quota of ${quota} notifications`, ExceptionCodes.RESOURCE_EXHAUSTED);
     this.name = 'SenderQuotaExceededException';
+  }
+}
+
+export class SenderNotValidException extends NotificationError {
+  constructor(sender: string) {
+    super(`Sender ${sender} is not valid`, ExceptionCodes.VALIDATION_ERROR);
+    this.name = 'SenderNotValidException';
   }
 }
