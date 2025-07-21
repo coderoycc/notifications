5d3d436c7d995a69256a16cf130a9931b27f3fff
rchambi <>
2025-07-20 22:54:34 -0400

    refactor: tenant loading ddd

--- src/notification/infra/senders/email-sender.adapter.ts
+++ src/notification/infra/senders/email-sender.adapter.ts
@@ -7,6 +7,7 @@ import { NotificationSender } from '@noti-domain/outbound-ports/notification.sen
 import { LoadDataFileTK } from 'src/tenant/infra/external/get-from-file.adapter';
 import { LoadTenantDataPort } from 'src/tenant/domain/outbound-ports/load-tenant-data';
 
+
 @Injectable()
 export class EmailSenderAdapter implements NotificationSender {
   constructor(
@@ -16,7 +17,7 @@ export class EmailSenderAdapter implements NotificationSender {
 
   async send(notification: Notification): Promise<SendResponse> {
     try {
-      const tenant = await this.getTenant.load('ADADA');
+      const tenant = await this.getTenant.load('tenant123');
       if (tenant === null) throw new Error('Tenant not found');
       console.log(tenant, 'Tentant details')
       return {} as SendResponse;
@@ -25,3 +26,5 @@ export class EmailSenderAdapter implements NotificationSender {
     }
   }
 }
+
+export const TKEmailSender = Symbol('TKEmailSender');
