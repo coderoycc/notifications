0f258bc38a9f30371e3e92d94cf6aaea28598f5e
rchambi <>
2025-07-23 22:43:46 -0400

    refactor: remove unused import in NotificationGetService and add spacing in GetTenantFromFileUseCase

--- src/notification/app/services/notification-get.service.ts
+++ src/notification/app/services/notification-get.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Notification } from "@noti-domain/entities/notification.entity";
-import { NotificationType } from "@noti-domain/entities/schemas";
 import { FindNotificationService, Paginate } from "@noti-domain/inbound-ports/find-notification.service";
 import { NotificationRepository } from "@noti-domain/outbound-ports/notification.repository";
 import { NOTIFICATION_REPOSITORY } from "@noti-infra/orm/repository/notification-typeorm.repository";
--- src/tenant/app/use-cases/get-from-file.use-case.ts
+++ src/tenant/app/use-cases/get-from-file.use-case.ts
@@ -3,6 +3,7 @@ import { Tenant } from 'src/tenant/domain/entities/tenant';
 import { GetTenantByCodePort } from 'src/tenant/domain/inbound-ports/get-tenant.port';
 import { LoadTenantDataPort } from 'src/tenant/domain/outbound-ports/load-tenant-data';
 import { LoadDataFileTK } from 'src/tenant/infra/external/get-from-file.adapter';
+
 @Injectable()
 export class GetTenantFromFileUseCase implements GetTenantByCodePort {
   constructor(@Inject(LoadDataFileTK) private readonly loader: LoadTenantDataPort) {}
