bcb1e38b923d501a1a5ee7533ece43a8d2b591f4
rchambi <>
2025-07-30 22:18:58 -0400

    feat: implement email and tenant services with nodemailer integration

--- src/notification/infra/senders/email-sender.adapter.ts
+++ src/notification/infra/senders/email-sender.adapter.ts
@@ -1,56 +1,19 @@
-import { Inject, Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer'; // O tu librería de email preferida
+import { Injectable } from '@nestjs/common';
 
 import { Notification } from '@noti-domain/entities/notification.entity';
 import { SendResponse } from '@noti-domain/dtos';
 import { NotificationSender } from '@noti-domain/ports/out/notification.sender';
-import { LoadDataFileTK } from 'src/tenant/infra/external/get-from-file.adapter';
-import { LoadTenantDataPort } from 'src/tenant/domain/outbound-ports/load-tenant-data';
 
 
 @Injectable()
 export class EmailSenderAdapter implements NotificationSender {
   constructor(
-    @Inject(LoadDataFileTK)
-    private readonly getTenant: LoadTenantDataPort,
   ) { }
-  private transporter:  nodemailer.Transporter;
 
-  private async initializeTransporter(tenantId: string): Promise<void> {
-    const tenant = await this.getTenant.load(tenantId);
-    if (tenant === null) throw new Error('Tenant not found');
-
-    this.transporter = nodemailer.createTransport({
-      host: 'host.example.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: tenant.email,
-        pass: tenant.pass,
-      },
-    });
-  }
   async send(notification: Notification): Promise<SendResponse> {
-    try {
-      await this.initializeTransporter('default');
-      const mailOptions = {
-        from: `NITIFICATION <${notification.createdBy}>`,
-        to: notification.target,
-        subject: notification.title,
-        text: notification.message,
-        html: `<p>${notification.message}</p>`,
-      };
-      await this.transporter.sendMail(mailOptions);
+    return new Promise(async (resolve, reject) => {
 
-      const sendResponse: SendResponse = {
-        success: true,
-        message: `Email sent successfully to ${notification.target}`,
-        notificationId: notification.id,
-      }
-      return sendResponse;
-    } catch (error) {
-      throw error;
-    }
+    })
   }
 }
 
