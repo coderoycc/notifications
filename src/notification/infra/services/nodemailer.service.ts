48eb81bcd39be29871398360cceb46f1b232a550
rchambi <>
2025-08-03 22:29:06 -0400

    feat: enhance NodemailerEmailService to initialize transporter with tenant credentials

--- src/notification/infra/services/nodemailer.service.ts
+++ src/notification/infra/services/nodemailer.service.ts
@@ -1,11 +1,30 @@
 import { Notification } from '@noti-domain/entities/notification.entity';
 import { EmailService } from '@noti-domain/ports/out/senders.services';
 import * as nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { GetTenantFromFileUseCase } from 'src/tenant/app/use-cases/get-from-file.use-case';
+import { Tenant } from 'src/tenant/domain/entities/tenant';
 
 export class NodemailerEmailService implements EmailService {
   private transporter: nodemailer.Transporter;
 
-  construtor(){}
+  constructor(private readonly tenantUc: GetTenantFromFileUseCase){
+    const dataTenant = this.tenantUc.execute('');
+    dataTenant.then((tenant: Tenant | null) => {
+      if (!tenant) {
+        throw new Error('Tenant not found');
+      }
+      this.transporter = nodemailer.createTransport({
+        host: 'smtp.example.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: tenant.name,
+          pass: tenant.pass,
+        },
+      } as SMTPTransport.Options);
+     });
+  }
 
   async sendEmail(noti: Notification): Promise<void> {
     await this.transporter.sendMail({
